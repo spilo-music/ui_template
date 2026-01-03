a1d2799c79a98556a291a197ea119e949f808211
SPILO <abemfermo@gmail.com>
2026-01-03 23:03:17 +0100

    update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,10 +4,16 @@ game 'gta5'
 name 'ui_template'
 author 'spilo.music'
 
+ui_page 'web/build/index.html'
+
 client_scripts {
     'client/main.lua'
 }
 
 server_scripts {
     'server/main.lua'
+}
+
+files {
+    "web/dist/**"
 }
